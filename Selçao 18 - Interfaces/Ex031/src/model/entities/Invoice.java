cdacefe3ebf1ea29e3ae81589f22036a8bcc3f23
João Nanuca dos Santos Neto <joaonanuca_petro@outlook.com>
2022-11-21 23:07:23 -0300

    Add files via upload
    Pequeno projeto (Sem a utilização de interface) divido em serviços para aprendizado de interfaces em Java

---
+++ Selçao 18 - Interfaces/Ex031/src/model/entities/Invoice.java
@@ -0,0 +1,32 @@
+package model.entities;
+
+/**
+ *
+ * @author joaon
+ */
+public class Invoice {
+
+    private Double basicPayment;
+    private Double tax;
+
+    public Invoice() {
+    }
+
+    public Invoice(Double basicPayment, Double tax) {
+        this.basicPayment = basicPayment;
+        this.tax = tax;
+    }
+
+    public Double getBasicPayment() {
+        return basicPayment;
+    }
+
+    public Double getTax() {
+        return tax;
+    }
+
+    public Double getTotalPayment() {
+        return getBasicPayment() + getTax();
+    }
+
+}
---
+++ Selçao 18 - Interfaces/Ex031/src/model/services/BrazilTaxService.java
@@ -0,0 +1,17 @@
+package model.services;
+
+/**
+ *
+ * @author joaon
+ */
+public class BrazilTaxService {
+
+    public Double tax(Double amount) {
+        if (amount <= 100.0) {
+            return amount * 0.2;
+        } else {
+            return amount * 0.15;
+        }
+    }
+
+}
